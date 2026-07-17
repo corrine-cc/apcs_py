ca745a6b89b17e5ae52ac29ad351b5d8ad9333f3
corrine-cc <mashu.tjmh@gmail.com>
2026-07-17 23:15:14 +0800

    a004.cpp
    題目：https://zerojudge.tw/ShowProblem?problemid=a004

---
+++ a004.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int a;
+while (cin>>a){
+    if((a%400==0)||(a%4==0 && a%100!=0)){
+        cout<<"閏年"<<endl;
+    }else{
+        cout<<"平年"<<endl;
+    }
+}
+}
